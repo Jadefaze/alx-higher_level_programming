7657e9fb0649c2f8315c2bfc11803da3d5040f41
Jadefaze <mabnoz@gmail.com>
2023-03-12 03:18:28 -0700

    first loop task

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+h1 = "The Zen of Python, by Tim Peters\n"
+l1 = "Beautiful is better than ugly."
+l2 = "Explicit is better than implicit."
+l3 = "Simple is better than complex."
+l4 = "Complex is better than complicated."
+l5 = "Flat is better than nested."
+l6 = "Sparse is better than dense."
+l7 = "Readability counts."
+l8 = "Special cases aren't special enough to break the rules."
+l9 = "Although practicality beats purity."
+l10 = "Errors should never pass silently."
+l11 = "Unless explicitly silenced."
+l12 = "In the face of ambiguity, refuse the temptation to guess."
+l13 = "There should be one-- and preferably only one --obvious way to do it."
+l14 = "Although that way may not be obvious at first unless you're Dutch."
+l15 = "Now is better than never."
+l16 = "Although never is often better than *right* now."
+l17 = "If the implementation is hard to explain, it's a bad idea."
+l18 = "If the implementation is easy to explain, it may be a good idea."
+l19 = "Namespaces are one honking great idea -- let's do more of those!"
+
+zen = [h1, l1, l2, l3, l4, l5, l6, l7, l8, l9, l10, l11, l12, l13,
+       l14, l15, l16, l17, l18, l19]
+
+for line in zen:
+    print(line)
