d97e614b1790041a902671b65100782a159c4682
Jadefaze <mabnoz@gmail.com>
2024-01-16 22:12:16 +0300

    casting str and ints

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit_str = str(number)[-1]
+last_digit_int = int(last_digit_str)
+
+if last_digit_int > 5:
+    print(f"The last digit of {number:d} is {last_digit_int}\
+ and is greater than 5")
+elif last_digit_int == 0:
+    print(f"The last digit of {number:d} is 0 and is 0")
+else:
+    print(f"The last digit of {number:d} is {last_digit_int}\
+ and is less than 6 and not 0")
