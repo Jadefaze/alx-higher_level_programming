130fc32aba33613902ac25dc847710307ffb3bc0
Jadefaze <mabnoz@gmail.com>
2024-01-17 21:52:27 +0300

    subscripting

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+for num in range(1, 100):
+    if num == 89:
+        print("{:02d}".format(num))
+    elif num < 10:
+        print("{:02d}".format(num), end=', ')
+    elif str(num)[0] < str(num)[1]:
+            print("{:02d}".format(num), end=', ')
+
