1b33e4d53112010ac687cf80bf863fa142dc0e2a
Jadefaze <mabnoz@gmail.com>
2024-01-19 02:05:59 +0300

    test

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4 as hidden
+    import hidden_4
     import sys
 
-    names = dir(hidden)
+    names = dir(hidden_4)
 
     for name in names:
         if name == '__*':
