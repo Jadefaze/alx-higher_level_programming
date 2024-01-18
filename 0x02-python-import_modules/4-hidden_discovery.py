2b911ac59840c6517eb8ee39f4934d64cd629078
Jadefaze <mabnoz@gmail.com>
2024-01-18 15:26:13 -0800

    dir()

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     names = dir(hidden_4)
 
     for name in names:
-        if name == '__*':
+        if name[:2] == "__":
             pass
         else:
             print(name)
