b9f73c35f6ed2c39bc59e43122105950c3b65a37
Jadefaze <mabnoz@gmail.com>
2024-01-20 13:17:20 +0300

    fixed initialisation of of int max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -12,7 +12,7 @@ def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
     else:
-        int_max = 0
+        int_max = my_list[0]
         for num in my_list:
             if num > int_max:
                 int_max = num
