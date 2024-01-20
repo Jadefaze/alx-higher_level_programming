5098b69ac1c8e675972c887705fe9cb2ec02636f
Jadefaze <mabnoz@gmail.com>
2024-01-20 13:46:05 +0300

    fixed null cases

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if not my_list:
-        return None
-    if idx is None or idx >= len(my_list):
+    if not my_list or idx is None or idx >= len(my_list):
         return None
 
-    return my_list[idx]
+    if 0 <= idx < len(my_list):
+        return my_list[idx]
+    else:
+        return None
