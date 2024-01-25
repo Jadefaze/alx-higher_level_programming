bdf0ca37ba6a4b24ef08405acbb190f68bd07316
Jadefaze <mabnoz@gmail.com>
2024-01-25 05:16:24 +0300

    max func for dictionaries

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,11 +5,6 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-    a_list = list(a_dictionary)
-
-    max_int = a_dictionary[a_list[0]]
-    for key in a_dictionary:
-        if a_dictionary[key] > max_int:
-            max_key = key
+    max_key = max(a_dictionary, key=a_dictionary.get)
 
     return max_key
