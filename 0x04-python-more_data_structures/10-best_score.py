2905c8c4230f8f4125b4f5cf1e11157f00585f98
Jadefaze <mabnoz@gmail.com>
2024-01-25 05:11:12 +0300

    dictionaries

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,15 +2,14 @@
 
 def best_score(a_dictionary):
 
-    if a_dictionary is None or not a_dictionary:
+    if not a_dictionary:
         return None
 
     a_list = list(a_dictionary)
 
     max_int = a_dictionary[a_list[0]]
-    for k, v in a_dictionary.items():
-        if v > max_int:
-            max_int = v
-            max_key = k
+    for key in a_dictionary:
+        if a_dictionary[key] > max_int:
+            max_key = key
 
     return max_key
