da9fe4f760c4fdd4fa25d3b86fb078a6dcc5216b
Jadefaze <mabnoz@gmail.com>
2024-02-24 22:26:19 +0300

    private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""To define a square class"""
+
+
+class Square:
+    """Represents a square."""
+
+    def __init__(self, size):
+        """instatiate with size (private)
+
+        Args:
+            size: size of the square (private)
+
+        """
+
+        self.__size = size
